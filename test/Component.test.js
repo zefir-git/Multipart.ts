2cc147624a49edb06d93b4015e333554abb3d1f9
Zefir Kirilov <contact@zefir.pro>
2024-09-25 13:34:57 +0300

    use TextEncode in Component tests

--- test/Component.test.js
+++ test/Component.test.js
@@ -1,5 +1,5 @@
 import {expect} from "chai";
-import {Component} from "../dist/index.js";
+import {Multipart, Component} from "../dist/index.js";
 
 describe("Component", () => {
 
@@ -35,7 +35,7 @@ describe("Component", () => {
         it("should parse headers and body correctly from Uint8Array", () => {
             const headers = "Content-Type: text/plain\r\nContent-Length: 5\r\n\r\n";
             const body = new Uint8Array([1, 2, 3, 4, 5]);
-            const data = new Uint8Array([...headers.split("").map(c => c.charCodeAt(0)), ...body]);
+            const data = Multipart.combineArrays([new TextEncoder().encode(headers), body]);
 
             const component = Component.parse(data);
 
@@ -57,7 +57,7 @@ describe("Component", () => {
 
         it("should handle headers with no body", () => {
             const headers = "Content-Type: text/plain\r\n\r\n";
-            const data = new Uint8Array([...headers.split("").map(c => c.charCodeAt(0))]);
+            const data = new TextEncoder().encode(headers);
 
             const component = Component.parse(data);
 
